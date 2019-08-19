268798a6a5d6d03eca51e8c1f31029f0e4fa8774
HamzaLee <Hamza.Litayem@yahoo.com>
2019-08-19 16:37:59 +0100

    Add custom interceptors

---
+++ DotNetCore.DI.Interception/DotNetCore.DI.Interception.Interceptors/DebuggerTryCatchWriterInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using Castle.DynamicProxy;
+
+namespace DotNetCore.DI.Interception.Interceptors
+{
+    public class DebuggerTryCatchWriterInterceptor : TryCatchWriterInterceptor
+    {
+        public override void Writer(string message) => Debug.WriteLine(message);
+        public override Func<IInvocation, string> OnSuccessMessage => invocation => $"Success {invocation.Method.Name}";
+        public override Func<IInvocation, Exception, string> OnFailureMessage => (invocation, exception) => $"Failed {invocation.Method.Name} {exception.Message}";
+
+    }
+}
